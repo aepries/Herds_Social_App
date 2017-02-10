0a3eb7fe25e47d9fd4b190c506dfc470e14625e3
aaronpries <ae.pries@gmail.com>
2017-02-10 13:33:33 -0700

    Working - Added Generic RV to Events Need to: change the content to the Firebase items

--- app/src/main/java/com/example/aaronpries/herds_social_app/EventsFragment.java
+++ app/src/main/java/com/example/aaronpries/herds_social_app/EventsFragment.java
@@ -6,16 +6,57 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by Admin on 04-06-2015.
  */
 public class EventsFragment extends Fragment {
 
-    @Nullable
+    private RecyclerView mBlogList;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+
+    public EventsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.events_fragment,container,false);
-        return v;
+//        View v = inflater.inflate(R.layout.events_fragment,container,false);
+        View rootView = inflater.inflate(R.layout.events_fragment,container,false);
+
+        RecyclerView rv = (RecyclerView) rootView.findViewById(R.id.blog_list);
+        rv.setHasFixedSize(true);
+        MyAdapter adapter = new MyAdapter(new String[]{"test one", "test two", "test three", "test four", "test five" , "test six" , "test seven"});
+        rv.setAdapter(adapter);
+
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        rv.setLayoutManager(llm);
+
+        return rootView;
+
+
     }
 }
