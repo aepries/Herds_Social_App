87ae4dc66b0510382a4dbbbe0b8bcde884bf452f
aaronpries <ae.pries@gmail.com>
2017-02-15 16:59:57 -0700

    Working - CardView Launches Title and Image Are Populated Also Created Detail Fragment but does not launch Also Added Colors Need to: Set onClickListener Create Groups Page

---
+++ app/src/main/java/com/example/aaronpries/herds_social_app/EventsExpandedFragment.java
@@ -0,0 +1,29 @@
+package com.example.aaronpries.herds_social_app;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EventsExpandedFragment extends Fragment {
+
+
+    public EventsExpandedFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.events_expanded_fragment, container, false);
+    }
+
+}
