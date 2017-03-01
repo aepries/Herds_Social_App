e66c332d49dba1fac15989b626bd147710ec4508
aaronpries <ae.pries@gmail.com>
2017-03-01 14:18:42 -0700

    Working - Added AddEvents Page -Added Firebase Storage -Added Image Uri -Posts to DB
    Need to:
    Link Groups and Category to store in DB

--- app/src/main/java/com/example/aaronpries/herds_social_app/AddEventsFragment.java
+++ app/src/main/java/com/example/aaronpries/herds_social_app/AddEventsFragment.java
@@ -1,11 +1,31 @@
 package com.example.aaronpries.herds_social_app;
 
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
+
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -14,16 +34,154 @@ import android.view.ViewGroup;
 public class AddEventsFragment extends Fragment {
 
 
+    private ImageButton mSelectImage;
+    private EditText mTitle;
+    private EditText mDesc;
+    private EditText mDate;
+    private Button mSubmit;
+    private Uri mImageUri = null;
+    private StorageReference mStorage;
+    private DatabaseReference mDatabase;
+    private ProgressDialog mProgress;
+    private MaterialBetterSpinner mSpinner;
+
+    private static  final int GALLERY_REQUEST = 1;
+    private static String TAG = "EventsFragment";
+
+
+
+//CREATE OPTIONS FOR DROPDOWN MENU (GROUP)
+    String[] mGroupList = {"Alpha Phi", "Alpha Sigma Alpha", "Delta Sigma Pi", "Geeks Who Drink", "Sigma Chi"};
+    String[] mCategoryList = {"Music", "Food", "Night Life", "Community", "Philanthropy"};
+
     public AddEventsFragment() {
         // Required empty public constructor
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.add_events_fragment, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        final View v = inflater.inflate(R.layout.add_events_fragment, container, false);
+        v.setTag(TAG);
+
+//MAKE REFERENCE TO DATABASE STORAGE FOR IMAGES (FOLDER)
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+//MAKE REFERENCE TO DATABASE ROOT DIRECTORY
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Data");
+
+
+//MAP FIELDS TO LAYOUT
+        mSelectImage = (ImageButton)v.findViewById(R.id.fieldImageButton);
+        mTitle = (EditText)v.findViewById(R.id.fieldEventTitle);
+        mDesc = (EditText)v.findViewById(R.id.fieldEventDescription);
+        mDate = (EditText)v.findViewById(R.id.fieldEventDate);
+        mSubmit = (Button)v.findViewById(R.id.btnAddEvent);
+
+//PROGRESS BAR
+        mProgress =  new ProgressDialog(getActivity());
+
+//DROPDOWN MENU (GROUPS)
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_dropdown_item_1line, mGroupList);
+        MaterialBetterSpinner textView = (MaterialBetterSpinner)v.findViewById(R.id.fieldGroup);
+        textView.setAdapter(adapter);
+
+//DROPDOWN MENU (CATEGORY)
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_dropdown_item_1line, mCategoryList);
+        MaterialBetterSpinner textView2 = (MaterialBetterSpinner)v.findViewById(R.id.fieldCategory);
+        textView2.setAdapter(adapter2);
+
+
+
+//CLICK LISTENER FOR IMAGE SELECTION
+        mSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public int hashCode() {
+                return super.hashCode();
+            }
+
+//REQUEST IMAGES FROM GALLERY
+            @Override
+            public void onClick(View view) {
+
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_REQUEST);
+
+            }
+        });
+
+//CLICK LISTENER FOR SUBMIT BUTTON
+        mSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startEventPost();
+
+            }
+        });
+
+
+        return v;
     }
+
+    private void startEventPost() {
+
+        mProgress.setMessage("Posting Event...");
+        mProgress.show();
+
+//GET DATA FROM FIELDS
+        final String title_value = mTitle.getText().toString().trim();
+        final String desc_value = mDesc.getText().toString().trim();
+        final String date_value = mDate.getText().toString().trim();
+
+//CHECK TO MAKE SURE FIELDS AREN'T EMPTY
+        if(!TextUtils.isEmpty(title_value)
+                && !TextUtils.isEmpty(desc_value)
+                && !TextUtils.isEmpty(date_value)
+                && mImageUri != null){
+
+            StorageReference filepath = mStorage.child("DATA").child(mImageUri.getLastPathSegment());
+
+            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+
+                    DatabaseReference newPost = mDatabase.push();
+
+                    newPost.child("title").setValue(title_value);
+                    newPost.child("info").setValue(desc_value);
+                    newPost.child("date").setValue(date_value);
+                    newPost.child("image").setValue(downloadUrl.toString());
+
+                    mProgress.dismiss();
+
+                }
+            });
+
+
+        }
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {
+
+            mImageUri = data.getData();
+
+            mSelectImage.setImageURI(mImageUri);
+
+        }
+    }
+
 
 }
