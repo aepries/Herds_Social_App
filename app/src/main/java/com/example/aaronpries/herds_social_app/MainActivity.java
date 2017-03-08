a2d0846a230e02cf53fafd9ac6f7c5e6afc2284f
aaronpries <ae.pries@gmail.com>
2017-03-08 13:21:10 -0700

    Working -Added Splash Screen - Added Category Page - Expand Events Page - Fixed Add Events Page -Added FB Login
    Need to:
    Get images to upload from local

--- app/src/main/java/com/example/aaronpries/herds_social_app/MainActivity.java
+++ app/src/main/java/com/example/aaronpries/herds_social_app/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.aaronpries.herds_social_app;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
@@ -34,6 +36,12 @@ import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+//    public MainActivity(){
+//        startProgress();
+//    }
+
     //Defining Variables
     private Toolbar toolbar;
     private NavigationView navigationView;
@@ -43,6 +51,19 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mBlogList;
     private DatabaseReference myRef;
     private DatabaseReference muhRef;
+    public boolean once = false;
+
+    public boolean isOnce() {
+        return once;
+    }
+
+    public void setOnce(boolean once) {
+        this.once = once;
+    }
+
+    public void setTrue(boolean once) {
+        setOnce(true);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,103 +74,128 @@ public class MainActivity extends AppCompatActivity {
         AppEventsLogger.activateApp(getApplication());
 
 
-        HomePage();
+        SharedPreferences settings = getSharedPreferences("prefs", 0);
+        boolean firstRun = settings.getBoolean("firstRun", false);
+        if (firstRun == false)//if running for first time
+        //Splash will load for first time
+        {
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putBoolean("firstRun", true);
+            editor.commit();
+            Intent i = new Intent(MainActivity.this, SplashScreen.class);
+            startActivity(i);
+            finish();
+        } else {
 
-        // Initializing Toolbar and setting it as the actionbar
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+            HomePage();
 
-        //Initializing NavigationView
-        navigationView = (NavigationView) findViewById(R.id.navigation_view);
-
-        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-
-            // This method will trigger on item Click of navigation menu
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-
-
-                //Checking if the item is in checked state or not, if not make it in checked state
-                if(menuItem.isChecked()) menuItem.setChecked(false);
-                else menuItem.setChecked(true);
-
-                //Closing drawer on item click
-                drawerLayout.closeDrawers();
-
-                //Check to see which item was being clicked and perform appropriate action
-                switch (menuItem.getItemId()){
+//        if (isOnce() == false){
+//            startProgress();
+//            setTrue(once);
+//        }
 
 
-                    //Replacing the main content with ContentFragment Which is our Inbox View;
-                    case R.id.events:
-                        EventsFragment Efragment = new EventsFragment();
-                        android.support.v4.app.FragmentTransaction EfragmentTransaction = getSupportFragmentManager().beginTransaction();
-                        EfragmentTransaction.replace(R.id.frame,Efragment);
-                        EfragmentTransaction.commit();
-                        return true;
+            //HomePage();
 
-                    // For rest of the options we just show a toast on click
+            // Initializing Toolbar and setting it as the actionbar
+            toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
 
-                    case R.id.groups:
-                        GroupsFragment Gfragment = new GroupsFragment();
-                        android.support.v4.app.FragmentTransaction GfragmentTransaction = getSupportFragmentManager().beginTransaction();
-                        GfragmentTransaction.replace(R.id.frame,Gfragment);
-                        GfragmentTransaction.commit();
-                        return true;
+            //Initializing NavigationView
+            navigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+            //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+            navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+                // This method will trigger on item Click of navigation menu
+                @Override
+                public boolean onNavigationItemSelected(MenuItem menuItem) {
 
 
-                    case R.id.categories:
-                        CategoriesFragment Cfragment = new CategoriesFragment();
-                        android.support.v4.app.FragmentTransaction CfragmentTransaction = getSupportFragmentManager().beginTransaction();
-                        CfragmentTransaction.replace(R.id.frame,Cfragment);
-                        CfragmentTransaction.commit();
-                        return true;
+                    //Checking if the item is in checked state or not, if not make it in checked state
+                    if (menuItem.isChecked()) menuItem.setChecked(false);
+                    else menuItem.setChecked(true);
 
-                    case R.id.profile:
-                        ProfileFragment Pfragment = new ProfileFragment();
-                        android.support.v4.app.FragmentTransaction PfragmentTransaction = getSupportFragmentManager().beginTransaction();
-                        PfragmentTransaction.replace(R.id.frame,Pfragment);
-                        PfragmentTransaction.commit();
-                        return true;
+                    //Closing drawer on item click
+                    drawerLayout.closeDrawers();
 
-                    default:
-                        Toast.makeText(getApplicationContext(),"Somethings Wrong",Toast.LENGTH_SHORT).show();
-                        return true;
+                    //Check to see which item was being clicked and perform appropriate action
+                    switch (menuItem.getItemId()) {
 
+
+                        //Replacing the main content with ContentFragment Which is our Inbox View;
+                        case R.id.events:
+                            EventsFragment Efragment = new EventsFragment();
+                            android.support.v4.app.FragmentTransaction EfragmentTransaction = getSupportFragmentManager().beginTransaction();
+                            EfragmentTransaction.replace(R.id.frame, Efragment);
+                            EfragmentTransaction.commit();
+                            return true;
+
+                        // For rest of the options we just show a toast on click
+
+                        case R.id.groups:
+                            GroupsFragment Gfragment = new GroupsFragment();
+                            android.support.v4.app.FragmentTransaction GfragmentTransaction = getSupportFragmentManager().beginTransaction();
+                            GfragmentTransaction.replace(R.id.frame, Gfragment);
+                            GfragmentTransaction.commit();
+                            return true;
+
+
+                        case R.id.categories:
+                            CategoriesFragment Cfragment = new CategoriesFragment();
+                            android.support.v4.app.FragmentTransaction CfragmentTransaction = getSupportFragmentManager().beginTransaction();
+                            CfragmentTransaction.replace(R.id.frame, Cfragment);
+                            CfragmentTransaction.commit();
+                            return true;
+
+                        case R.id.profile:
+                            ProfileFragment Pfragment = new ProfileFragment();
+                            android.support.v4.app.FragmentTransaction PfragmentTransaction = getSupportFragmentManager().beginTransaction();
+                            PfragmentTransaction.replace(R.id.frame, Pfragment);
+                            PfragmentTransaction.commit();
+                            return true;
+
+                        default:
+                            Toast.makeText(getApplicationContext(), "Somethings Wrong", Toast.LENGTH_SHORT).show();
+                            return true;
+
+                    }
                 }
-            }
-        });
+            });
 
-        // Initializing Drawer Layout and ActionBarToggle
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer, R.string.closeDrawer){
+            // Initializing Drawer Layout and ActionBarToggle
+            drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+            ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer) {
 
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
-                super.onDrawerClosed(drawerView);
-            }
+                @Override
+                public void onDrawerClosed(View drawerView) {
+                    // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                    super.onDrawerClosed(drawerView);
+                }
 
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+                @Override
+                public void onDrawerOpened(View drawerView) {
+                    // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
 
-                super.onDrawerOpened(drawerView);
-            }
-        };
+                    super.onDrawerOpened(drawerView);
+                }
+            };
 
-        //Setting the actionbarToggle to drawer layout
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+            //Setting the actionbarToggle to drawer layout
+            drawerLayout.addDrawerListener(actionBarDrawerToggle);
 
-        //calling sync state is necessay or else your hamburger icon wont show up
-        actionBarDrawerToggle.syncState();
-
+            //calling sync state is necessay or else your hamburger icon wont show up
+            actionBarDrawerToggle.syncState();
 
 
+        }
+    }
 
 
+    private void startProgress() {
 
+        Intent intent = new Intent((MainActivity.this), SplashScreen.class);
+        startActivity(intent);
     }
 
     @Override
@@ -190,4 +236,5 @@ public class MainActivity extends AppCompatActivity {
 //        super.onDestroy();
 //        firebaseRecyclerAdapter.cleanup();
 //    }
+
 }
